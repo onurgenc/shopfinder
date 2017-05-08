e5930b3bec1059b2b76540611a5a5276e5d18813
Onur Genç <onurgenc@gmail.com>
2017-05-09 00:10:40 +0300

    added missing files

---
+++ Helper/Data.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright © 2017 Ogenc . All rights reserved.
+ */
+namespace Ogenc\Shopfinder\Helper;
+class Data extends \Magento\Framework\App\Helper\AbstractHelper
+{
+
+	/**
+     * @param \Magento\Framework\App\Helper\Context $context
+     */
+	public function __construct(\Magento\Framework\App\Helper\Context $context
+	) {
+		parent::__construct($context);
+	}
+}
